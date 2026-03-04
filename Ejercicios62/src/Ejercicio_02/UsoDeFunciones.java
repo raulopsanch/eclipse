44c7a6c1a7ea2e859ebd9b101f7868c4f0f8c768
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-03-04 22:12:02 +0100

    Ejercicios62_ejercicio_02

---
+++ Ejercicios62/src/Ejercicio_02/UsoDeFunciones.java
@@ -0,0 +1,21 @@
+package Ejercicio_02;
+
+
+
+public class UsoDeFunciones {
+
+	public static void main(String[] args) {
+		double numero = 13;
+		
+		// Raíz cuadrada del número
+		System.out.println("Raíz cuadrada de " + numero + "= " + Math.sqrt(numero));
+		
+		// Potencia 3 de número
+		System.out.println("13^3= " + Math.pow(numero, 3));
+		
+		// Número aleatorio menor a 100
+		int aleatorio = nexInt
+
+	}
+
+}
