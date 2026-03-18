5fc990cad68bccfd9c03b9d197d513da9e247d33
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-03-18 19:01:14 +0100

    todo_recuperado

---
+++ trabajo_documentacion/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"trabajo_documentacion","l":"Estado","k":"9"},{"p":"trabajo_documentacion","l":"Main"},{"p":"trabajo_documentacion","l":"Pedido"},{"p":"trabajo_documentacion","l":"Producto"},{"p":"trabajo_documentacion","l":"Servicio"}];updateSearchResults();
