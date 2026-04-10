fab7e7e529b66406ef758f2437577429b574913f
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-04-10 20:36:59 +0200

    actividad74_ejercicio_01_01

--- tema_7/src/actividad72/ejercicio_01/GestionPersona.java
+++ tema_7/src/actividad72/ejercicio_01/GestionPersona.java
@@ -3,12 +3,12 @@ package actividad72.ejercicio_01;
 import java.util.*;
 
 public class GestionPersona {
-    private Map<Integer, Persona>personas;
+    private HashMap<Integer, Persona>personas;
 
     public GestionPersona(){
-
+        this.personas = new HashMap<>();
     }
 
 
-    public 
+    public
 }
