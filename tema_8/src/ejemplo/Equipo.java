f3974fd40b0742e96dc13538101a574af9e81cb5
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-04-22 21:20:05 +0200

    examen5 y tema8

---
+++ tema_8/src/ejemplo/Equipo.java
@@ -0,0 +1,18 @@
+package ejemplo;
+
+public class Equipo {
+    private String nombre;
+    private String estadio;
+    private int aniofundation;
+    private String ciudad;
+
+
+    public Equipo(String nombre, String estadio, int aniofundation, String ciudad) {
+        this.nombre = nombre;
+        this.estadio = estadio;
+        this.aniofundation = aniofundation;
+        this.ciudad = ciudad;
+    }
+
+    
+}
