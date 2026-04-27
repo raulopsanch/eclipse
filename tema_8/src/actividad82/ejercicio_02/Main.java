ec0704d653b126b776e7aa1498fc97bd6e2821b3
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-04-27 20:36:37 +0200

    cambio actividad82_ejercicio_02

--- tema_8/src/actividad82/ejercicio_02/Main.java
+++ tema_8/src/actividad82/ejercicio_02/Main.java
@@ -2,7 +2,7 @@ package actividad82.ejercicio_02;
 
 public class Main {
     public static void main(String[] args) {
-        Suma total = (int a, int b) -> { return a + b; };
+        Suma total = (int a, int b) -> ( a + b );
 
         System.out.println(total.suma(10, 20));
     }
