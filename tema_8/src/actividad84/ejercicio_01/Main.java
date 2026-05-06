40de535ac91eafe58a2e047b2a3408fea7cf5318
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-05-06 11:22:06 +0200

    Hola

--- tema_8/src/actividad84/ejercicio_01/Main.java
+++ tema_8/src/actividad84/ejercicio_01/Main.java
@@ -31,7 +31,7 @@ public class Main {
         System.out.println("Buscar alumno");
         System.out.println("-------------");
         //Buscar alumno
-        buscarAlumno(alumnos, entrada);
+        System.out.print(buscarAlumno(alumnos, entrada));
     }
 
 
