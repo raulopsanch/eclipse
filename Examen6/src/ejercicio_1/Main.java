732875ff15c759b5d913fafbcfd7d548b603b86e
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-05-12 19:30:06 +0200

    examen6

---
+++ Examen6/src/ejercicio_1/Main.java
@@ -0,0 +1,7 @@
+package ejercicio_1;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Examen6/src/ejercicio_3/Main.java
@@ -0,0 +1,7 @@
+package ejercicio_3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
